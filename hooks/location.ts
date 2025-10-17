2adcdb3ed99a1406cc15b4462b686e60024f561a
whizydan <whizydan@gmail.com>
2025-10-17 13:10:00 +0300

    removed throw exception

--- hooks/location.ts
+++ hooks/location.ts
@@ -1,9 +1,24 @@
 import * as Location from 'expo-location';
+import { Alert } from 'react-native';
 
 export async function sendLocation() {
   const { status } = await Location.requestForegroundPermissionsAsync();
-  if (status !== 'granted') throw new Error('Location permission denied');
+  if (status !== 'granted') {
+    Alert.alert(
+      'Permission Required',
+      'Location permission is needed to get your position. Please enable it in settings.',
+      [{ text: 'OK' }]
+    );
+    return null;
+  }
 
-  const { coords } = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
-  return coords
+  try {
+    const { coords } = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+    return coords;
+  } catch (err) {
+    Alert.alert('Location Error', 'Unable to retrieve your location.');
+    return null;
+  }
 }
