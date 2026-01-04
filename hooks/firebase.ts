9fd7e167b1a3bf3c4499483304c99e6dac608b77
whizydan <whizydan@gmail.com>
2026-01-04 11:12:16 +0300

    add firebase config

--- hooks/firebase.ts
+++ hooks/firebase.ts
@@ -1,21 +1,21 @@
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBjvLHis8U7-t-byOmpl00vaO15nylSu3c",
+  authDomain: "roadmateassist-b7ebe.firebaseapp.com",
+  projectId: "roadmateassist-b7ebe",
+  storageBucket: "roadmateassist-b7ebe.firebasestorage.app",
+  messagingSenderId: "986834107956",
+  appId: "1:986834107956:web:4cbab79dd36d2a3e5b9461",
+  measurementId: "G-7PP8Z9D97Q"
 };
 
-const app = initializeApp(firebaseConfig);
 
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
+  
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
 
 export default app;
